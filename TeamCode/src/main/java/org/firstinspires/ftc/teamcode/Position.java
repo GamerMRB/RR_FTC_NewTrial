51f36112cb95a662b661cb8fdac43fc860f4419a
Larbagar <darcyvilkner@gmail.com>
2024-10-14 16:30:14 -0400

    Created a function to get the direction of a position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Position.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Position.java
@@ -3,9 +3,14 @@ package org.firstinspires.ftc.teamcode;
 public class Position {
     final public Vec2 disp;
     final public double rot;
+    final public Vec2 dirInternal;
     public Position(Vec2 dispIn, double rotIn){
         disp = dispIn;
         rot = rotIn;
+        dirInternal = Vec2.polar(1, rot);
+    }
+    public Vec2 dir(){
+        return dirInternal;
     }
     public Position add(Position pos){
         return new Position(disp.add(pos.disp.rotate(rot)), rot + pos.rot);
