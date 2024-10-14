415b388eadea7d4b33f91cf21400da55ddad2c5b
Larbagar <darcyvilkner@gmail.com>
2024-10-14 16:50:28 -0400

    Created a PID class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+public class PID {
+    public double p;
+    public double i;
+    public double d;
+    public double eLast = 0;
+    public double eDiff = 0;
+    public double eInt = 0;
+    public boolean skipDiff = true;
+
+    public PID(double p, double i, double d){
+       this.p = p;
+       this.i = i;
+       this.d = d;
+    }
+    public void update(double e, double dt){
+        eInt += e * dt;
+        if(skipDiff){
+            skipDiff = false;
+        }else{
+            eDiff = e - eLast;
+        }
+        eLast = e;
+    }
+    public double getPow(){
+        return p * eLast + i * eInt + d * eDiff;
+    }
+}
