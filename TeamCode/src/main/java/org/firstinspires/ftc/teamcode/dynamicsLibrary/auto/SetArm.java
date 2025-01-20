027d0213892b84a8e73a9a35394de8b3b62b53ca
GamerMRB <mikeryanblessing@gmail.com>
2025-01-20 18:15:31 -0500

    setArm is active

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/SetArm.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.dynamicsLibrary.auto;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.dynamicsLibrary.UscOpMode;
+
+public class SetArm extends Instruction{
+    /**
+     * Number of ticks to go
+     */
+    double theta;
+    public SetArm(double theta) {
+        this.theta = theta;
+    }
+
+    public void start(UscOpMode opMode) {
+        opMode.armPivot.setTargetPosition((int) (theta * 538 / (2 * Math.PI)));
+        opMode.armPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public boolean update(UscOpMode opMode) {
+        return opMode.armPivot.isBusy();
+    }
+}
