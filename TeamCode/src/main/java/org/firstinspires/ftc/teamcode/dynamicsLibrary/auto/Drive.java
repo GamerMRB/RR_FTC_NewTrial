bf4d55d49d298ffc394ead1ebbff28c6c004ac17
UnfortunatelyNo <94651734+UnfortunatelyNo@users.noreply.github.com>
2025-01-20 17:32:34 -0500

    Auto Driving Instruction complete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/Drive.java
@@ -8,7 +8,14 @@ public class Drive extends Instruction {
     public Drive(Vec2 diff){
         this.diff = diff;
     }
+
     public boolean update(UscOpMode opMode){
-        return true;
+        double angle = diff.angle();
+        opMode.frontLeft.setVelocity((opMode.MAX_VELOCITY * ((Math.sin(angle) + Math.cos(angle))/Math.sqrt(2))));
+        opMode.frontRight.setVelocity((opMode.MAX_VELOCITY * ((Math.cos(angle) - Math.sin(angle))/Math.sqrt(2))));
+        opMode.backLeft.setVelocity((opMode.MAX_VELOCITY * ((Math.cos(angle) - Math.sin(angle))/Math.sqrt(2))));
+        opMode.backRight.setVelocity((opMode.MAX_VELOCITY * ((Math.sin(angle) + Math.cos(angle))/Math.sqrt(2))));
+        double avgPos = (double) (opMode.frontLeft.getCurrentPosition() + opMode.frontRight.getCurrentPosition() + opMode.backLeft.getCurrentPosition() + opMode.backRight.getCurrentPosition()) /4;
+        return (Math.abs(((avgPos / opMode.TICKS_PER_REVOLUTION) * opMode.WHEEL_CIRCUMFERENCE) - (diff.mag() * Math.cos(angle))) <= 10);
     }
 }
