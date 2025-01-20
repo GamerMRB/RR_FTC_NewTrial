3c34d932cf2f379aa4d2a944ffc16a92b6dea438
Larbagar <darcyvilkner@gmail.com>
2025-01-20 18:06:24 -0500

    Fixed Aaryan's TERRIBLE code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/Wait.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/Wait.java
@@ -3,16 +3,14 @@ package org.firstinspires.ftc.teamcode.dynamicsLibrary.auto;
 public class Wait extends Instruction{
     long time;
     long targetTime;
-    boolean timeCheck = true;
     public Wait(long nanoTime){
         this.time = nanoTime;
     }
 
+    public void start(){
+        targetTime = System.nanoTime() + time;
+    }
     public boolean update(){
-        if (timeCheck){
-            targetTime = System.nanoTime() + time;
-            timeCheck = false;
-        }
         return (System.nanoTime() > targetTime);
     }
 }
