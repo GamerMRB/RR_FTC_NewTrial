bb7db2b57afcc0a0ca54770985f8b322e17548e3
Larbagar <darcyvilkner@gmail.com>
2025-01-21 17:28:36 -0500

    Added SeriesInstruction

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/SeriesInstruction.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.dynamicsLibrary.auto;
+
+import org.firstinspires.ftc.teamcode.dynamicsLibrary.UscOpMode;
+
+public class SeriesInstruction extends Instruction {
+    Instruction[] instructions;
+    int instructionIndex = 0;
+    boolean startInstruction = true;
+    SeriesInstruction(Instruction[] instructions){
+        this.instructions = instructions;
+    }
+    public boolean update(UscOpMode opMode){
+        if(startInstruction){
+            if(instructionIndex == this.instructions.length){
+                return true;
+            }
+            this.instructions[instructionIndex].start(opMode);
+            startInstruction = false;
+        }
+        boolean endInstruction = this.instructions[instructionIndex].update(opMode);
+        if(endInstruction){
+            this.instructions[instructionIndex].end(opMode);
+            instructionIndex++;
+            startInstruction = true;
+        }
+        return false;
+    }
+}
