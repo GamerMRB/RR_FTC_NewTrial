0c9b8a85bd5a23f05deebc9608975b6f0e566092
Larbagar <darcyvilkner@gmail.com>
2025-01-21 17:47:10 -0500

    Fixed CombinedInstruction continuing to run instructions after they're complete.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/CombinedInstruction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/CombinedInstruction.java
@@ -2,10 +2,15 @@ package org.firstinspires.ftc.teamcode.dynamicsLibrary.auto;
 
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.UscOpMode;
 
+import java.util.Arrays;
+
 public class CombinedInstruction extends Instruction {
     Instruction[] instructions;
+    boolean[] instructionsFinished;
     CombinedInstruction(Instruction[] instructions){
         this.instructions = instructions;
+        instructionsFinished = new boolean[this.instructions.length];
+        Arrays.fill(instructionsFinished, false);
     }
 
     public void start(UscOpMode opMode) {
@@ -16,8 +21,15 @@ public class CombinedInstruction extends Instruction {
 
     public boolean update(UscOpMode opMode) {
         boolean done = true;
-        for(Instruction instruction : instructions){
-            if(!instruction.update(opMode)){
+        for(int i = 0; i < instructions.length; i++){
+            if(instructionsFinished[i]){
+                continue;
+            }
+            boolean instructionDone = instructions[i].update(opMode);
+            if(instructionDone){
+                instructions[i].end(opMode);
+                instructionsFinished[i] = true;
+            }else{
                 done = false;
             }
         }
