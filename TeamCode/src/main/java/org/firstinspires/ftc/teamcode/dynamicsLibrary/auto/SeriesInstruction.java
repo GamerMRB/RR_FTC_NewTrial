f10a5a0af29690ef75f24eb4a24cd31d725dfa4c
Larbagar <darcyvilkner@gmail.com>
2025-02-03 15:35:21 -0500

    Revert "Created repeated"
    This reverts commit bd08740456af3c17a3d279b2dd23a901829234ae.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/SeriesInstruction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/SeriesInstruction.java
@@ -4,25 +4,20 @@ import org.firstinspires.ftc.teamcode.dynamicsLibrary.UscOpMode;
 
 public class SeriesInstruction extends Instruction {
     Instruction[] instructions;
-    int instructionIndex;
-    boolean startInstruction;
+    int instructionIndex = 0;
+    boolean startInstruction = true;
     public SeriesInstruction(Instruction[] instructions){
         this.instructions = instructions;
     }
-
-    public void start(UscOpMode opMode) {
-        instructionIndex = 0;
-        startInstruction = true;
-    }
     public boolean update(UscOpMode opMode){
         if(startInstruction){
-            if(instructionIndex == instructions.length){
+            if(instructionIndex == this.instructions.length){
                 return true;
             }
             this.instructions[instructionIndex].start(opMode);
             startInstruction = false;
         }
-        boolean endInstruction = instructions[instructionIndex].update(opMode);
+        boolean endInstruction = this.instructions[instructionIndex].update(opMode);
         if(endInstruction){
             this.instructions[instructionIndex].end(opMode);
             instructionIndex++;
