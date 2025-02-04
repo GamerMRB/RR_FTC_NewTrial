1bbeb036e71fe5132cdbd8e77a92f5c7875126f8
GamerMRB <mikeryanblessing@gmail.com>
2025-02-04 18:04:55 -0500

    removed telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/SetArmLength.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/SetArmLength.java
@@ -10,23 +10,11 @@ public class SetArmLength extends Instruction{
         targetPos = length;
     }
     public void start(UscOpMode opMode) {
-//        opMode.armSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        if (targetPos < 0) {
-//            opMode.armSlide.setPower(-1);
-//        } else {
-            opMode.armSlide.setPower(1);
-//        }
+        opMode.armSlide.setPower(1);
         opMode.armSlide.setTargetPosition((int) (targetPos * opMode.TICKS_PER_REVOLUTION/ ((48 * Math.PI * 2) / 25.4)));
         opMode.armSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
     public boolean update(UscOpMode opMode){
-        opMode.telemetry.clear();
-        opMode.telemetry.addLine(Boolean.toString(opMode.armSlide.isBusy()));
-        opMode.telemetry.addLine(String.valueOf(opMode.armSlide.getCurrentPosition()));
-        opMode.telemetry.addLine(String.valueOf(opMode.armSlide.getTargetPosition()));
-        opMode.telemetry.addLine(opMode.armSlide.getMode().toString());
-        opMode.telemetry.addLine(String.valueOf(opMode.armSlide.getPower()));
-        opMode.telemetry.update();
         return !opMode.armSlide.isBusy();
     }
 
