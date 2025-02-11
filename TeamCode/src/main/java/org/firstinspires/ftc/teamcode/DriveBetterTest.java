9e38b79069c1139b1b2928192fd1d411b0c8e708
Larbagar <darcyvilkner@gmail.com>
2025-02-11 18:36:00 -0500

    Made drive easier easier

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBetterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBetterTest.java
@@ -12,6 +12,7 @@ import org.firstinspires.ftc.teamcode.dynamicsLibrary.Vec2;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.CombinedInstruction;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.Drive;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.DriveBetter;
+import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.DriveEasier;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.Instruction;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.LinearPath;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.Log;
@@ -48,9 +49,7 @@ public class DriveBetterTest extends UscOpMode {
 
         executeInstructions(new Instruction[]{
                 new Wait((long) Math.pow(10, 9)),
-                new DriveBetter(
-                    new LinearPath(Vec2.xy(0, 3000), 1000, 1000)
-                )
+                new DriveEasier(1000, 0),
         });
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/DriveEasier.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/DriveEasier.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.dynamicsLibrary.auto;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.Vec2;
 
 public class DriveEasier extends DriveBetter {
-    public DriveEasier(Vec2 diff){
-        super(new LinearPath(diff, 1000, 1000));
+    public DriveEasier(double x, double y){
+        super(new LinearPath(Vec2.xy(x, y), 1000, 1000));
     }
 }
