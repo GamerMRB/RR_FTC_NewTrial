6a1069da00eb325e41f96b76ab5e982e402795aa
GamerMRB <mikeryanblessing@gmail.com>
2025-02-11 19:04:23 -0500

    we can dream again!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCrusher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCrusher.java
@@ -1,13 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.teamcode.dynamicsLibrary.Vec2.xy;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.UscOpMode;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.CombinedInstruction;
-import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.Drive;
+import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.DriveEasier;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.Instruction;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.Log;
 import org.firstinspires.ftc.teamcode.dynamicsLibrary.auto.SeriesInstruction;
@@ -27,6 +25,7 @@ public class AutoCrusher extends UscOpMode {
         setUpHardware();
         waitForStart();
 
+        imu.resetYaw();
         armPivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
@@ -40,7 +39,7 @@ public class AutoCrusher extends UscOpMode {
 
                 new SeriesInstruction(new Instruction[]{
                     new Wait(1000000000),
-                    new Drive(xy(600, 0)),
+                    new DriveEasier(600, 0),
                 })
             }),
 
@@ -51,9 +50,9 @@ public class AutoCrusher extends UscOpMode {
                 new Log("Setup pushing"),
                 new SetArmAngle(0),
                 new SeriesInstruction(new Instruction[]{
-                        new Wait(750000000),
-                        new Drive(xy(0, 675)),
-                        new Drive(xy(625, 0))
+                    new Wait(750000000),
+                    new DriveEasier(0, -675),
+                    new DriveEasier(675, 0)
                 })
             })
         ));
@@ -64,59 +63,53 @@ public class AutoCrusher extends UscOpMode {
                 instructions.add(
                     new SeriesInstruction(new Instruction[]{
                         new Log("Push sample" + (i + 1)),
-                        new Drive(xy(0, 225)),
-                        new Drive(xy(-1000, 0)),
+                        new DriveEasier(0, -225),
+                        new DriveEasier(-1050, 0),
                         new Log("Drive to the sample #2"),
-                        new Drive(xy(0, -610)),
+                        new DriveEasier(0, 610),
                     })
                 );
             }
 
             if ((i == 0) || (i == 1)) {
                 instructions.add(
-                        new SeriesInstruction(new Instruction[]{
-                                new Log("Push sample" + (i + 1)),
-                                new Drive(xy(0, 225)),
-                                new Drive(xy(-1000, 0)),
-                                new Drive(xy(1000, 0))
-                        })
+                    new SeriesInstruction(new Instruction[]{
+                        new Log("Push sample" + (i + 1)),
+                        new DriveEasier(0, -275),
+                        new DriveEasier(-1100, 0),
+                        new DriveEasier(1100, 0)
+                    })
                 );
             }
         }
 
-        // TODO: Fix the code below next meeting
         //Second, Third, Fourth, Fifth Sample
-        for (int i = 0; i <= 3; i ++) {
+        for (int i = 0; i <= 3; i++) {
             instructions.add(
                 new SeriesInstruction(new Instruction[]{
+                    new SetTurn(Math.PI),
                     new CombinedInstruction(new Instruction[]{
-                        new Log("Position to grab specimen #" + (i + 2)),
-                        new SetTurn(Math.PI),
-                        new SetArmAngle(5.5),
+                            new SetClaw(true),
+                            new Log("Position to grab specimen # " + i),
+                            new SetArmLength(0),
+                            new SetArmAngle(6.5),
                     }),
 
                     new CombinedInstruction(new Instruction[]{
-                        new Log("Prepare for scoring specimen #" + (i + 2)),
-                        new SetClaw(true),
-                        new SetArmAngle(14.75),
-                        new SetTurn(Math.PI),
-                        new SetArmLength(7),
+                            new Log("Prepare for scoring specimen # " + i),
+                            new SetTurn(0),
+                            new SetArmAngle(13),
+                            new SetArmLength(13),
+                            new Log("Drive and score specimen # " + i),
+                            new SeriesInstruction(new Instruction[]{
+                                new DriveEasier(457, 1000 + (i * 50)),
+                            }),
                     }),
 
                     new CombinedInstruction(new Instruction[]{
-                        new Log("Drive and score specimen #" + (i + 2)),
-                        new SeriesInstruction(new Instruction[]{
-                                new Drive(xy(0, -1219 - (i * 50))),
-                                new Drive(xy(457, 0)),
-                        }),
-                    }),
-
-                    new CombinedInstruction(new Instruction[]{
-                        new Log("Drive back and reset"),
+                        new Log("Drive back and reset for specimen# " + (i + 1)),
                         new SetClaw(false),
-                        new SetArmLength(0),
-                        new Drive(xy(0, -1220 - (i * 50))),
-                        new Drive(xy(-750, 0)),
+                        new DriveEasier(-500, -(1000 + (i * 50))),
                     })
                 })
             );
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/DriveEasier.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamicsLibrary/auto/DriveEasier.java
@@ -4,6 +4,6 @@ import org.firstinspires.ftc.teamcode.dynamicsLibrary.Vec2;
 
 public class DriveEasier extends DriveBetter {
     public DriveEasier(double x, double y){
-        super(new LinearPath(Vec2.xy(x, y), 1000, 1000));
+        super(new LinearPath(Vec2.xy(x, y), 1500, 1000));
     }
 }
